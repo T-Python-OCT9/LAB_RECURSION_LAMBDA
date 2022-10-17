afc598d24523124bc521e57579af6d6586b63466
Samaa <Samaanawaf@gmail.com>
2022-10-17 19:11:42 +0300

    modified the lambda task and correct it

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 ## 1) Using recursion, if given a word/phrase return how many vowels(a,e,i,o,u) are in that phrase or word:
 def VowelCount(s):
     ''' this function will take the input and check if it has a vowels in it and retrun the number of how many the string have in '''
-
     vowels = "aeiouAEIOU"
     if s == "":
         return 0
@@ -10,12 +9,12 @@ def VowelCount(s):
     else:
         return 0 + VowelCount(s[1:])
 
-print("This statment (Ilove python) has in it : ",VowelCount("iLOve python",),"vowels in it .")
+print("This statment (Ilove python) has in it : ",VowelCount("iLOve python"),"vowels in it .")
 
 # create a new list containing each number from the previous list mutliplied by itself.
  
 multiply_list = [40,35, 10, 15, 20] 
 
-multiply_result = list(map(lambda nums : nums*2 ,multiply_list))
+multiply_result = list(map(lambda nums : nums*nums ,multiply_list))
 
-print(multiply_result) 
+print("The result of multply the list is : ",multiply_result) 
